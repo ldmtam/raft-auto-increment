fe4a0687c6a6b06a288bd1b37cb5691242ce4d4e
TamLe <ldmtam@gmail.com>
2020-01-04 16:03:23 +0700

    add Backup function, change other function name to AutoIncrement database interface

--- database/interface.go
+++ database/interface.go
@@ -2,9 +2,11 @@ package database
 
 // AutoIncrement represents the AutoIncrement database interface
 type AutoIncrement interface {
-	GetSingle(key string) (uint64, error)
-	GetMultiple(key string, quantity uint64) ([]uint64, error)
-	GetLast(key string) (uint64, error)
+	GetOne(key string) (uint64, error)
+	GetMany(key string, quantity uint64) ([]uint64, error)
+	GetLastInserted(key string) (uint64, error)
+
+	Backup() ([]byte, error)
 
 	Close() error
 }
